e7479042283b611f94923c8b1209234d12e13e6e
Atulsaiyan <109258174+atulsubedi@users.noreply.github.com>
2022-07-15 11:07:53 +0545

    js imported

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const username = prompt("may i have your name?")
-alert("this script works fine " + username )
+const translate =document.querySelector("#btn-translate")
+const language= document.querySelector("#minion-talk")
+console.log(language)
+translate.addEventListener("click", button
+ )
+
+function button(){
+    console.log('clicked!!')
+    console.log("input",language.value)
+}
+
