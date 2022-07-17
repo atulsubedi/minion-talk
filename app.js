65db26e0c48a6a2de96767c9010685197fb15292
Atulsaiyan <109258174+atulsubedi@users.noreply.github.com>
2022-07-17 12:11:12 +0545

    everything

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const translate =document.querySelector("#btn-translate")
-const language= document.querySelector("#minion-talk")
-console.log(language)
-translate.addEventListener("click", button
- )
+    const language= document.querySelector("#minion-talk")
+    const output= document.querySelector("#output")
+    const ServerUrl = "https://api.funtranslations.com/translate/minion.json"
 
-function button(){
-    console.log('clicked!!')
-    console.log("input",language.value)
-}
 
+    function GetTranslated(text){
+    return ServerUrl + "?" + "text=" + text
+    }
+
+    function errorhandler(error){
+     console.log("error occur", error)
+     alert("error occur please try again later")
+    }
+
+    function ClickHandler(){
+    const inputText = language.value; 
+    fetch(GetTranslated(inputText))
+    .then(response => response.json())
+    .then(json => {
+    const translation = json.contents.translated;
+    output.innerText = translation;})
+    .catch(errorhandler)}
+    translate.addEventListener("click",ClickHandler)
